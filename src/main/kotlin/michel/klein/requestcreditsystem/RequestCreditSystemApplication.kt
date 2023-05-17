2a4dec95de8f63edc0f6cb1866d9ad3990ea8813
Michel Klein <michel.kps@outlook.com>
2023-05-17 09:46:20 -0300

    feat: initial commit

---
+++ src/main/kotlin/michel/klein/requestcreditsystem/RequestCreditSystemApplication.kt
@@ -0,0 +1,11 @@
+package michel.klein.requestcreditsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestCreditSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestCreditSystemApplication>(*args)
+}
---
+++ src/test/kotlin/michel/klein/requestcreditsystem/RequestCreditSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package michel.klein.requestcreditsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RequestCreditSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
